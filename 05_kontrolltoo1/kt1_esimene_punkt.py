2148351080cdeea49f22cc3293c413877e9abcf2
Aksel Aisting <aaisting@tlu.ee>
2026-03-05 11:39:10 +0200

    Create kt1_esimene_punkt.py

---
+++ 05_kontrolltoo1/kt1_esimene_punkt.py
@@ -0,0 +1,10 @@
+import math
+def arvutus_esimene_punkt():
+    arv_1=2
+    arv_2=4
+
+    korrutis_ruutjuur = math.sqrt(arv_2 * arv_1)
+    print ("korrutatud arvude ruutjuur on:",korrutis_ruutjuur)
+
+arvutus_esimene_punkt()
+
